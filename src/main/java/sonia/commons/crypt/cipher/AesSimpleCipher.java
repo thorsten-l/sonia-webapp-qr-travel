78801e0158abe5ee4225aa0743aa09303cc09bbd
Thorsten Ludewig <t.ludewig@gmail.com>
2022-02-25 12:58:47 +0100

    Apache-2.0 license - header

--- src/main/java/sonia/commons/crypt/cipher/AesSimpleCipher.java
+++ src/main/java/sonia/commons/crypt/cipher/AesSimpleCipher.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 Thorsten Ludewig (t.ludewig@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package sonia.commons.crypt.cipher;
 
 //~--- JDK imports ------------------------------------------------------------
@@ -18,7 +33,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
- * @author Thorsten Ludewig (t.ludewig@ostfalia.de)
+ * @author Thorsten Ludewig (t.ludewig@gmail.com)
  * @since 1.1.0
  */
 public final class AesSimpleCipher implements SimpleCipher
@@ -241,7 +256,7 @@ public final class AesSimpleCipher implements SimpleCipher
   /**
    * Class description
    *
-   * @author Thorsten Ludewig (t.ludewig@ostfalia.de)
+   * @author Thorsten Ludewig (t.ludewig@gmail.com)
    */
   public static final class Builder
   {
